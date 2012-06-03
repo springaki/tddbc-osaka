6e5ca3cdea64cc011aca0bbdbd08f4c39a0c685b
Kenichiroh Ota <oota_ken@hotmail.com>
2012-06-03 15:30:21 +0900

    もうっちょっとがんばろう

--- drink.rb
+++ drink.rb
@@ -1,3 +1,13 @@
+# coding: utf-8
 class Drink
+  attr_accessor :name
+  attr_accessor :price 
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
 end
+
+COKE = Drink.new("コーラ", 120)
 
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require './vending_machine'
+require './drink'
 require './money'
 
 describe VendingMachine do
@@ -51,8 +52,16 @@ describe VendingMachine do
 end
 
 describe Drink do
-  before do
-    @drinik = Drink.new
+
+  it "コーラの名前と単価を取得する" do
+    coke = Drink.new("コーラ", 120)
+    coke.name.should == "コーラ"
+    coke.price.should == 120
+  end
+
+  it "コーラのマスター作っちゃう" do
+    COKE.name.should == "コーラ"
+    COKE.price.should == 120
   end
 
 end
