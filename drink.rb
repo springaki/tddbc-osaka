aca3a31b59fce5d22c37b4a269aaf68670cade25
Kenichiroh Ota <oota_ken@hotmail.com>
2012-06-03 15:59:43 +0900

    もうっちょっとがんばろう

--- drink.rb
+++ drink.rb
@@ -1,13 +1,13 @@
 # coding: utf-8
 module Drink
-class Drink
-  attr_accessor :name
-  attr_accessor :price 
+  class Drink
+    attr_accessor :name
+    attr_accessor :price 
 
-  def initialize(name, price)
-    @name = name
-    @price = price
+    def initialize(name, price)
+      @name = name
+      @price = price
+    end
   end
-end
-COKE = Drink.new("コーラ", 120)
+  COKE = Drink.new("コーラ", 120)
 end
