30081ef0e5731b87073c6d136b9e7e994dcdb3a5
Aki <aki.onetime@gmail.com>
2012-06-03 14:59:24 +0900

    総計実装

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -11,6 +11,7 @@ describe VendingMachine do
   end
   it "自動販売機には、50円玉を投入できる" do
     @vending_machine.throw(Money::GOJUUEN).should == "success"
+    @vending_machine.total.should == 50
   end
   it "自動販売機には、100円玉を投入できる" do
     @vending_machine.throw(Money::HYAKUEN).should == "success"
@@ -27,6 +28,7 @@ describe VendingMachine do
   it "自販機にはお金を複数回投入できる" do
     @vending_machine.throw(Money::JUUEN)
     @vending_machine.throw(Money::JUUEN)
+    @vending_machine.total.should == 20
   end
   it "自販機に投入された金額の総計を取得できる" do
     @vending_machine.throw(Money::JUUEN)
--- vending_machine.rb
+++ vending_machine.rb
@@ -1,18 +1,27 @@
 class VendingMachine
+	attr_accessor :amount
+					
+	def initialize
+		@amount = 0
+	end
+
   def throw(amount)
 	  if amount == Money::JUUEN or 
        amount == Money::GOJUUEN or
        amount == Money::HYAKUEN or
        amount == Money::GOHYAKUEN or
        amount == Money::SENEN
+			@amount += amount
       "success"
     else
       "failure"
     end
   end
+
   def total
-		Money::JUUEN
+		@amount
   end
+
   def withdrow
   end
 end
